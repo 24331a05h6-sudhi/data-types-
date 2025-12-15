29f82011afeea05f81e7bcfeae30358fa12cd59c
24331a05h6-sudhi <24331a05h6@mvgrce.edu.in>
2025-12-15 20:09:21 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+integer_value = 18
+print("Integer value:", integer_value)
+print("Type of value:", type(integer_value))
+
+char_value = 'V'
+print("Char value:", char_value)
+print("Type of value:", type(char_value))
+
+string_value = "virat"
+print("String value:", string_value)
+print("Type of value:", type(string_value))
+
+float_value = 1.8001
+print("Float value:", float_value)
+print("Type of value:", type(float_value))
